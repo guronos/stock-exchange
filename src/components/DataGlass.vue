8095d4cc6f57ef06a8d819cef0f1484b10aac908
Vadim <chub960@mail.com>
2023-05-29 11:53:23 +0300

    edit computed in DataGlass.vue

--- src/components/DataGlass.vue
+++ src/components/DataGlass.vue
@@ -4,7 +4,7 @@
       <caption class="bg-blue-grey-darken-2">
         Биржевой стакан
         {{
-          tikersInfo
+          store.getters.showTikers
         }}
       </caption>
       <thead class="bg-blue-grey-darken-2">
@@ -13,13 +13,13 @@
         <th>Всего<v-divider /></th>
       </thead>
       <tbody class="text-body-2">
-        <AllOrders :colorClass="'text-deep-orange-darken-3'" :orders="asks" :userOrders="userOrdersAsks" />
+        <AllOrders :colorClass="'text-deep-orange-darken-3'" :orders="asks" :userOrders="store.getters.showUserOrdersAsks" />
         <tr class="bg-blue-grey-lighten-1" v-if="spred">
           <td>Spred</td>
           <td>{{ Number(spred) }}%</td>
           <td></td>
         </tr>
-        <AllOrders :colorClass="'text-green-accent-4'" :orders="bids" :userOrders="userOrdersBids" />
+        <AllOrders :colorClass="'text-green-accent-4'" :orders="store.getters.showOrdersBids" :userOrders="store.getters.showUserOrdersBids" />
       </tbody>
     </v-table>
   </div>
@@ -31,9 +31,5 @@ import AllOrders from './AllOrders.vue';
 
 const store = useStore();
 const asks = computed(() => store.getters.showOrdersAsks.reverse());
-const bids = computed(() => store.getters.showOrdersBids);
-const spred = computed(() => ((store.getters.showOrdersAsks[store.getters.showOrdersAsks.length - 1]?.[0] - store.getters.showOrdersBids[0]?.[0])/ store.getters.showOrdersAsks[store.getters.showOrdersAsks.length - 1]?.[0] * 100).toFixed(3)) ;
-const userOrdersAsks = computed(() => store.getters.showUserOrdersAsks);
-const userOrdersBids = computed(() => store.getters.showUserOrdersBids);
-const tikersInfo = computed(()=> store.getters.showTikers)
+const spred = computed(() => ((store.getters.showOrdersAsks[store.getters.showOrdersAsks.length - 1]?.[0] - store.getters.showOrdersBids[0]?.[0])/ store.getters.showOrdersAsks[store.getters.showOrdersAsks.length - 1]?.[0] * 100).toFixed(3));
 </script>
